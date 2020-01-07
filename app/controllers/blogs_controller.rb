3801ce5546356b637018697b651fbf0018a7ae4f
坂本裕樹 <hiroki@Hiroki-Macbook.local>
2020-01-07 21:15:50 +0900

    [Finish] CARAVAN 1 to 7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,16 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
+
+  def index
+
+    #記事を全件取得
+    @blogs = Blog.all
+  end
+
+
 
   def new
   	  @blog = Blog.new
@@ -17,7 +26,22 @@ class BlogsController < ApplicationController
 
 
   def edit
+    @blog = Blog.find(params[:id])
   end
+
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blogs_path(blog)
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+
 
  private
  def blog_params
